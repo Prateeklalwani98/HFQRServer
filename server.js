1ff5a398040a9bd87e133086e18ab957597fbb96
Prateek Lalwani <143477785+Prateeklalwani98@users.noreply.github.com>
2024-01-17 14:06:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,77 @@
+// import express from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+
+// const app = express();
+// const port = 5000;
+
+// const dbURL = "mongodb+srv://prateeklalwani005:s02ag0FrhFKBpzSQ@test-db.kiq2tsr.mongodb.net";
+// const dbName = "HFQR";
+
+// mongoose.connect(`${dbURL}/${dbName}`)
+
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB:", err);
+//   });
+
+// const ItemsSchema = new mongoose.Schema({
+//   listName: String,
+//   url: String,
+// });
+
+// const Item = mongoose.model("Items", ItemsSchema);
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.post("/addItem", async (req, res) => {
+//   const { url, listName } = req.body;
+
+//   const newItem = new Item({ url, listName });
+
+//   try {
+//     // Save the item to the database
+//     await newItem.save();
+//     console.log("Item added to MongoDB");
+
+//     // Return the saved item (including the URL)
+//     res.status(200).json(newItem);
+//   } catch (err) {
+//     console.error("Error adding item to MongoDB:", err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+// app.get("/getLastSavedUrl", async (req, res) => {
+//   try {
+//     // Find the last saved item and return its URL
+//     const lastSavedItem = await Item.findOne().sort({ _id: -1 }).limit(1);
+//     res.json({ lastSavedUrl: lastSavedItem ? lastSavedItem.url : null });
+//   } catch (err) {
+//     console.error("Error fetching last saved URL from MongoDB:", err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+// // app.get("/", (req, res) => {
+// //   res.send("Hello World");
+// // });
+
+// // For Express server example
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+// app.listen(port, () => {
+//   console.log(`Connected to server port: ${port}`);
+// });
+
+// --------------------------------- New code below:
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -9,7 +83,6 @@ const dbURL = "mongodb+srv://prateeklalwani005:s02ag0FrhFKBpzSQ@test-db.kiq2tsr.
 const dbName = "HFQR";
 
 mongoose.connect(`${dbURL}/${dbName}`)
-
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -17,38 +90,46 @@ mongoose.connect(`${dbURL}/${dbName}`)
     console.error("Error connecting to MongoDB:", err);
   });
 
-const ItemsSchema = new mongoose.Schema({
-  listName: String,
-  url: String,
+const CxDetailsSchema = new mongoose.Schema({
+  CxName: {
+    type: String,
+    required: true,
+  },
+  CxPhoneNumber: {
+    type: String,
+    required: true,
+  },
+  CxEmail: String,
 });
 
-const Item = mongoose.model("Items", ItemsSchema);
+const CxDetails = mongoose.model("CxDetails", CxDetailsSchema);
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/addItem", async (req, res) => {
-  const { url, listName } = req.body;
+app.post("/addCxDetails", async (req, res) => {
+  const { CxName, CxPhoneNumber, CxEmail } = req.body;
 
-  const newItem = new Item({ url, listName });
+  if (!CxName || !CxPhoneNumber) {
+    return res.status(400).send("Please enter name & phone number and try again.");
+  }
+
+  const newCxDetails = new CxDetails({ CxName, CxPhoneNumber, CxEmail });
 
   try {
-    // Save the item to the database
-    await newItem.save();
-    console.log("Item added to MongoDB");
+    await newCxDetails.save();
+    console.log("CxDetails added to MongoDB");
 
-    // Return the saved item (including the URL)
-    res.status(200).json(newItem);
+    res.status(200).json(newCxDetails);
   } catch (err) {
-    console.error("Error adding item to MongoDB:", err);
+    console.error("Error adding CxDetails to MongoDB:", err);
     res.status(500).send("Internal Server Error");
   }
 });
 
 app.get("/getLastSavedUrl", async (req, res) => {
   try {
-    // Find the last saved item and return its URL
-    const lastSavedItem = await Item.findOne().sort({ _id: -1 }).limit(1);
+    const lastSavedItem = await CxDetails.findOne().sort({ _id: -1 }).limit(1);
     res.json({ lastSavedUrl: lastSavedItem ? lastSavedItem.url : null });
   } catch (err) {
     console.error("Error fetching last saved URL from MongoDB:", err);
@@ -56,16 +137,11 @@ app.get("/getLastSavedUrl", async (req, res) => {
   }
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// For Express server example
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
 app.listen(port, () => {
   console.log(`Connected to server port: ${port}`);
 });
+
